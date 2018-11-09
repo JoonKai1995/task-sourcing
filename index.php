66a631fdd6715822f885e3d02e681bcefccf87b2
Daniel <tanjoonkai@gmail.com>
2018-11-09 14:45:42 +0800

    added index.php

---
+++ index.php
@@ -0,0 +1,230 @@
+<?php
+include("header.php");
+?>
+
+<!DOCTYPE HTML>
+<!--
+	Concept by gettemplates.co
+	Twitter: http://twitter.com/gettemplateco
+	URL: http://gettemplates.co
+-->
+<html>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Task Sourcing</title>
+	</head>
+
+	<body>
+		
+	<div class="fh5co-loader"></div>
+	
+	<div id="page">
+	<nav class="fh5co-nav" role="navigation">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-2 text-left">
+					<div id="fh5co-logo"><a href="index.html">TaskSourcing<span>.</span></a></div>
+				</div>
+                <div class="col-xs-10 text-right menu-1">
+	                <ul>
+	                <li class="active"><a href="index.html"><i class="icon-home"></i></a></li>
+                    </ul>
+                </div>
+				<!-- <div class="col-xs-10 text-right menu-1">
+					<ul>
+						<li class="active"><a href="index.html">Home</a></li>
+                        <li><a href="createNewUser.html">Create New Account</a></li>
+                        <li class="has-dropdown">
+                            <a href="welcome.html">Login</a>
+                            <ul class="dropdown">
+                                <li><a href="userLogin.html">User Login</a></li>
+                                <li><a href="adminLogin.html">Administrator Login</a></li>
+                            </ul>
+                        </li>
+						<li><a href="products.html">Products</a></li>
+						<li><a href="about.html">About</a></li>
+						<li><a href="blog.html">Blog</a></li>
+						<li><a href="contact.html">Contact</a></li>
+					</ul>
+				</div> -->
+			</div>
+		</div>
+	</nav>
+
+	<header id="fh5co-header" class="fh5co-cover" role="banner" style="background-image:url(images/background.jpg);" data-stellar-background-ratio="0.5">
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-7 text-left">
+					<div class="display-t">
+						<div class="display-tc animate-box" data-animate-effect="fadeInUp">
+							<h1 class="mb30">Need some help with your tasks?</h1>
+							<p>
+								<a href="welcome.php" class="btn btn-primary">Get Started</a>
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+
+	<!-- <div id="fh5co-project">
+		<div class="container">
+			<div class="row row-pb-md">
+				<div class="col-md-8 col-md-offset-2 text-left fh5co-heading  animate-box">
+					<span>news</span>
+					<h2>Recent Tasks Added</h2>
+					<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-md-4 col-sm-6 fh5co-project animate-box" data-animate-effect="fadeIn">
+					<a href="#"><img src="images/work-1.jpg" alt="Free HTML5 Website Template by gettemplates.co" class="img-responsive">
+						<div class="fh5co-copy">
+							<h3>Clipboard Office</h3>
+							<p>Web Design</p>
+						</div>
+					</a>
+				</div>
+				<div class="col-md-4 col-sm-6 fh5co-project animate-box" data-animate-effect="fadeIn">
+					<a href="#"><img src="images/work-2.jpg" alt="Free HTML5 Website Template by gettemplates.co" class="img-responsive">
+						<div class="fh5co-copy">
+							<h3>Smart Layers</h3>
+							<p>Brand &amp; Identity</p>
+						</div>
+					</a>
+				</div>
+				<div class="col-md-4 col-sm-6 fh5co-project animate-box" data-animate-effect="fadeIn">
+					<a href="#"><img src="images/work-3.jpg" alt="Free HTML5 Website Template by gettemplates.co" class="img-responsive">
+						<div class="fh5co-copy">
+							<h3>Notepad Mockup</h3>
+							<p>Illustration</p>
+						</div>
+					</a>
+				</div>
+
+			</div>
+		</div>
+	</div> -->
+
+
+
+	<div id="fh5co-services" class="fh5co-bg-section border-bottom">
+		<div class="container">
+			<div class="row row-pb-md">
+				<div class="col-md-8 col-md-offset-2 text-left animate-box" data-animate-effect="fadeInUp">
+					<div class="fh5co-heading">
+						<span>We're here to help</span>
+						<h2>How it Works</h2>
+						<p>Getting swamped, or in need of cash? Have no fear.</p>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-4 col-sm-6 ">
+					<div class="feature-center animate-box" data-animate-effect="fadeInUp">
+						<span class="icon">
+							<i class="icon-folder2"></i>
+						</span>
+						<h3>Consolidation</h3>
+						<p>All tasks are displayed. <br>Users can create their own.</p>
+					</div>
+				</div>
+				<div class="col-md-4 col-sm-6 ">
+					<div class="feature-center animate-box" data-animate-effect="fadeInUp">
+						<span class="icon">
+							<i class="icon-price-tag"></i>
+						</span>
+						<h3>Bidding Feature</h3>
+						<p>Bid on the tasks you want to complete, and get paid for it at the end!</p>
+					</div>
+				</div>
+				<div class="clearfix visible-sm-block"></div>
+				<div class="col-md-4 col-sm-6 ">
+					<div class="feature-center animate-box" data-animate-effect="fadeInUp">
+						<span class="icon">
+							<i class="icon-users"></i>
+						</span>
+						<h3>User-Driven</h3>
+						<p>Tasks are submitted by users, for users. <br>Users are the priority.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+        
+    <div id="fh5co-team">
+		<div class="container">
+			<div class="row animate-box row-pb-md">
+				<div class="col-md-8 col-md-offset-2 text-left fh5co-heading">
+					<span>a dedicated team</span>
+					<h2>Meet the Developers</h2>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-4 col-sm-4 animate-box" data-animate-effect="fadeIn">
+					<div class="fh5co-staff">
+						<img src="images/Daniel.jpg" alt="Free HTML5 Templates by gettemplates.co">
+						<h3>Tan Joon Kai, Daniel</h3>
+						<strong class="role">Back-end Developer</strong>
+						<p>Bachelor of Computer Science <br>National University of Singapore</p>
+					</div>
+				</div>
+				<div class="col-md-4 col-sm-4 animate-box" data-animate-effect="fadeIn">
+					<div class="fh5co-staff">
+						<img src="images/Pearlissa.jpeg" alt="Free HTML5 Templates by gettemplates.co">
+						<h3>Pearlissa Taim Qi Yah</h3>
+						<strong class="role">Front-end Developer</strong>
+						<p>Bachelor of Information Security <br>National University of Singapore</p>
+					</div>
+				</div>
+				<div class="col-md-4 col-sm-4 animate-box" data-animate-effect="fadeIn">
+					<div class="fh5co-staff">
+						<img src="images/Zheng_Kai.jpg" alt="Free HTML5 Templates by gettemplates.co">
+						<h3>Lim Zheng Kai</h3>
+						<strong class="role">Back-end Developer</strong>
+						<p>Bachelor of Computer Science <br>National University of Singapore</p>
+					</div>
+				</div>
+			</div>
+            <div class="row">
+				<div class="col-md-4 col-sm-4 animate-box" data-animate-effect="fadeIn">
+					<div class="fh5co-staff">
+						<img src="images/Samuel.jpg" alt="Free HTML5 Templates by gettemplates.co">
+						<h3>Lee Boon Howe, Samuel</h3>
+						<strong class="role">Back-end Developer</strong>
+						<p>Backelor of Information Systems <br>National University of Singapore</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>    
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
+	</div>
+	
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- countTo -->
+	<script src="js/jquery.countTo.js"></script>
+	<!-- Magnific Popup -->
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/magnific-popup-options.js"></script>
+	<!-- Stellar -->
+	<script src="js/jquery.stellar.min.js"></script>
+	<!-- Main -->
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
+
